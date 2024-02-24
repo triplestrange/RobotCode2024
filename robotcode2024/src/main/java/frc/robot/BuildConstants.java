4f8a9364915ecd656d1180fde0dd86b5ea2f1a40
Lori Brown <triplestrange@guilford.edu>
2024-02-24 11:07:03 -0500

    Create BuildConstants.java

---
+++ robotcode2024/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "robotcode2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 25;
+  public static final String GIT_SHA = "c860f4635c61593bae6da6ef3e60728397f38aab";
+  public static final String GIT_DATE = "2024-02-19 18:26:29 EST";
+  public static final String GIT_BRANCH = "alternate-encoder";
+  public static final String BUILD_DATE = "2024-02-19 20:50:12 EST";
+  public static final long BUILD_UNIX_TIME = 1708393812899L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
