6dff63694d513b42a33a06ad17f5019866ea020b
Kemchobro <vidhatu@gmail.com>
2024-03-26 23:41:33 -0400

    Update Superstructure.java

--- robotcode2024/src/main/java/frc/robot/subsystems/Superstructure.java
+++ robotcode2024/src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,28 +1,50 @@
-package frc.robot;
+package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
 
-public class StateManager extends SubsystemBase {
+public class Superstructure extends SubsystemBase {
 
     RobotContainer m_robotcontainer;
 
-    public StateManager(RobotContainer m_robotcontainer) {
+    private Goal currentGoal = Goal.STOW;
+    private Goal desiredGoal = Goal.STOW;
+    private Goal lastGoal = Goal.STOW;
+
+    private final Arm arm;
+    private final Climber climber;
+    private final BackpackActuator backpackActuator;
+
+    private Timer goalTimer = new Timer();
+
+    public Superstructure(RobotContainer m_robotcontainer) {
         this.m_robotcontainer = m_robotcontainer;
+
+        setDefaultCommand(setGoalCommand(Goal.STOW));
+        goalTimer.start();
+    }
+
+    public enum GoalState {
+        AIM,
+        AMP,
+        TRAP,
+        STOW,
+        CLIMB,
+        GROUND,
+        SOURCE,
+        OFF
     }
 
     @Override
     public void periodic() {
-
+        switch () {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
     }
 
-    public enum state {
-        SHOOT,
-        AMP,
-        TRAP,
-        STOW,
-        TEST,
-        CLIMB,
-        GROUND,
-        SOURCE,
-    }
 }
